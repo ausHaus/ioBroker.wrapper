ee4542f43362eb6395d1840b8fd8118b300abff7
Haus <vasilijus@live.com>
2017-07-03 18:20:23 +0300

    Create words.js

---
+++ admin/words.js
@@ -0,0 +1,17 @@
+systemDictionary = {
+    "Home adapter settings": {"en": "Home adapter settings", "de": "Home Adapter-Einstellungen", "ru": "Настройки драйвера Home"},
+    "Name":                  {"en": "Name",                  "de": "Name",                       "ru": "Имя"},
+    "Group":                 {"en": "Group",                 "de": "Gruppe",                     "ru": "Группа"},
+    "ID":                    {"en": "ID",                    "de": "ID",                         "ru": "ID"},
+    "Role":                  {"en": "Role",                  "de": "Rolle",                      "ru": "Роль"},
+    "Room":                  {"en": "Room",                  "de": "Raum",                       "ru": "Комната"},
+    "Function":              {"en": "Function",              "de": "Funktion",                   "ru": "Функция"},
+    "Binds":                 {"en": "Binds",                 "de": "Bindet",                     "ru": "Связать"},
+    "Add device: ":          {"en": "Add device: ",          "de": "Add device: ",               "ru": "Добавить устройство: "},
+    "Filter: ":              {"en": "Filter: ",              "de": "Filter: ",                   "ru": "Фильтр: "},
+    "on save adapter restarts with new config immediately": {
+        "en": "On save adapter restarts with new config immediately.",
+        "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
+        "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями."
+    }
+};
